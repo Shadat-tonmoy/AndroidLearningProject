0c61bb149ac9e87a21e4121e7c6cc6366a474415
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-05-20 17:33:56 +0600

    implemented click listener functionalities with data bindings

--- DataBindingDemo/app/src/main/java/com/shadattonmoy/databindingdemo/MainActivity.java
+++ DataBindingDemo/app/src/main/java/com/shadattonmoy/databindingdemo/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.shadattonmoy.databindingdemo.dataSource.DummyDataSource;
 import com.shadattonmoy.databindingdemo.databinding.ActivityMainBinding;
@@ -11,6 +13,7 @@ import com.shadattonmoy.databindingdemo.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity
 {
     ActivityMainBinding dataBinder;
+    MainActivityClickListener clickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,5 +27,28 @@ public class MainActivity extends AppCompatActivity
     {
         dataBinder = DataBindingUtil.setContentView(this,R.layout.activity_main);
         dataBinder.setPerson(DummyDataSource.getDummyPerson());
+        clickListener = new MainActivityClickListener();
+        dataBinder.setClickListener(clickListener);
+    }
+
+    public class MainActivityClickListener
+    {
+        public void onEmailButtonClicked(View view)
+        {
+            Toast.makeText(view.getContext(), "ClickedOnEmail", Toast.LENGTH_SHORT).show();
+
+        }
+
+        public void onPhoneButtonClicked(View view)
+        {
+            Toast.makeText(view.getContext(), "ClickedOnPhone", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onMessageButtonClicked(View view)
+        {
+            Toast.makeText(view.getContext(), "ClickedOnMessage", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
