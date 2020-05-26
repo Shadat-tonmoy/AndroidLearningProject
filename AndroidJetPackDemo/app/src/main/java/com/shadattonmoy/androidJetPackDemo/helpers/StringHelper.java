b4d27e5b96377b98ffbaa603485bb7e72784a164
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-05-26 12:29:20 +0600

    experimented with more features of databinding like include, binding value from id references etc

---
+++ AndroidJetPackDemo/app/src/main/java/com/shadattonmoy/androidJetPackDemo/helpers/StringHelper.java
@@ -0,0 +1,9 @@
+package com.shadattonmoy.androidJetPackDemo.helpers;
+
+public class StringHelper
+{
+    public static String capitalizeString(String text)
+    {
+        return text.toUpperCase();
+    }
+}
